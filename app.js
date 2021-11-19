04d42325620dd9db038c992b5c7e48547c45ea2f
Zemola <azeemolatunji@gmail.com>
2021-11-19 08:55:35 +0100

    Made changes as suggested by the reviwer

--- app.js
+++ app.js
@@ -20,63 +20,63 @@ closeMenu.addEventListener('click', closeMobileMenu);
 // The speker Selection
 const data = [
   {
-    id: "1",
-    pic: "./image/jazzy.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "Michael Collins Ajereh",
-    designation: "Southafrican Air news Magazine Author and Editor with 20years experience",
+    id: '1',
+    pic: './image/jazzy.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'Michael Collins Ajereh',
+    designation: 'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "Professionally known as Don Jazzy, is a Nigerian record producer,and philanthropist. He is the founder and the CEO of Mavin Records.",
+      'Professionally known as Don Jazzy, is a Nigerian record producer,and philanthropist. He is the founder and the CEO of Mavin Records.',
   },
   {
-    id: "2",
-    pic: "./image/Olamide.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "Olamide Gbenga Adedeji",
+    id: '2',
+    pic: './image/Olamide.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'Olamide Gbenga Adedeji',
     designation:
-      "Southafrican Air news Magazine Author and Editor with 20years experience",
+      'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "Professionally known as Olamide is a Nigerian hip hop recording artist. He records in Yoruba and English.",
+      'Professionally known as Olamide is a Nigerian hip hop recording artist. He records in Yoruba and English.',
   },
   {
-    id: "3",
-    pic: "./image/Davido.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "David Adedeji Adeleke",
+    id: '3',
+    pic: './image/Davido.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'David Adedeji Adeleke',
     designation:
-      "Southafrican Air news Magazine Author and Editor with 20years experience",
+      'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "Professionally known as Davido, is a Nigerian-American singer, songwriter, and record producer. Davido rose to fame after releasing Dami Duro.",
+      'Professionally known as Davido, is a Nigerian-American singer, songwriter, and record producer. Davido rose to fame after releasing Dami Duro.',
   },
   {
-    id: "4",
-    pic: "./image/wizkid.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "Ayodeji Ibrahim Balogun",
+    id: '4',
+    pic: './image/wizkid.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'Ayodeji Ibrahim Balogun',
     designation:
-      "Southafrican Air news Magazine Author and Editor with 20years experience",
+      'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "Professionally known as Wizkid is a Nigerian singer and songwriter. He began recording music at the age of 11.",
+      'Professionally known as Wizkid is a Nigerian singer and songwriter. He began recording music at the age of 11.',
   },
   {
-    id: "5",
-    pic: "./image/burnaboy.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "Damini Ebunoluwa Ogulu",
+    id: '5',
+    pic: './image/burnaboy.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'Damini Ebunoluwa Ogulu',
     designation:
-      "Southafrican Air news Magazine Author and Editor with 20years experience",
+      'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "Professionally known as Burna Boy, is a Nigerian singer, rapper and songwriter. He is one of the biggest and most successful African artists.",
+      'Professionally known as Burna Boy, is a Nigerian singer, rapper and songwriter. He is one of the biggest and most successful African artists.',
   },
   {
-    id: "7",
-    pic: "./image/Tiwa-Savage.jpg",
-    altText: "speaker image",
-    nameOfSpeaker: "Tiwa Savage",
+    id: '7',
+    pic: './image/Tiwa-Savage.jpg',
+    altText: 'speaker image',
+    nameOfSpeaker: 'Tiwa Savage',
     designation:
-      "Southafrican Air news Magazine Author and Editor with 20years experience",
+      'Southafrican Air news Magazine Author and Editor with 20years experience',
     history:
-      "A Nigerian singer, songwriter and actress. Born in Isale Eko, she relocated to London at the age of 11 for her secondary education.",
+      'A Nigerian singer, songwriter and actress. Born in Isale Eko, she relocated to London at the age of 11 for her secondary education.',
   },
 ];
 
@@ -107,8 +107,8 @@ window.onload = () => {
     speakerName.textContent = speakerPara.nameOfSpeaker;
     contentSection.appendChild(speakerName);
 
-    const speakerDesignation = document.createElement("p");
-    speakerDesignation.setAttribute("class", "speaker-designation");
+    const speakerDesignation = document.createElement('p');
+    speakerDesignation.setAttribute('class', 'speaker-designation');
     speakerDesignation.textContent = speakerPara.designation;
     contentSection.appendChild(speakerDesignation);
 
